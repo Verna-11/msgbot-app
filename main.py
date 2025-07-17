604c9e603487374e608a6bfcec6ea65a66c8b67c
Verna-11 <vernardo.barrera28@gmail.com>
2025-07-17 19:52:25 +0800

    add port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import sqlite3, requests, re
+import os
 
 app = Flask(__name__)
 
@@ -136,5 +137,7 @@ def dashboard():
     return render_template("dashboard.html", orders=orders, seller=seller)
 
 # Start the app
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
