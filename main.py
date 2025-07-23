6442fa1189136a55f203fd9ca5cf0cbb1af91597
Verna-11 <vernardo.barrera28@gmail.com>
2025-07-24 03:50:08 +0800

    referrals

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def init_pg():
         )
     ''')
 
-    cur.executed('''
+    cur.execute('''
         CREATE TABLE referrals (
             id SERIAL PRIMARY KEY,
             seller TEXT NOT NULL,
